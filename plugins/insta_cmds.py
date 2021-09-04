992ab252dde1451466a22d929ec4cbb66dbf5aa9
MuFaz-TG <88883729+BXBotz@users.noreply.github.com>
2021-09-04 11:25:14 +0530

    Update insta_cmds.py

--- plugins/insta_cmds.py
+++ plugins/insta_cmds.py
@@ -86,8 +86,8 @@ async def post(bot, message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Photos", callback_data=f"photos#{username}"),
-                        InlineKeyboardButton("Videos", callback_data=f"video#{username}")
+                        InlineKeyboardButton("📸 Pʜᴏᴛᴏs", callback_data=f"photos#{username}"),
+                        InlineKeyboardButton("📽 Vɪᴅᴇᴏs", callback_data=f"video#{username}")
                     ]
                 ]
             )
@@ -124,8 +124,8 @@ async def igtv(bot, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Yes", callback_data=f"yesigtv#{username}"),
-                    InlineKeyboardButton("No", callback_data=f"no#{username}")
+                    InlineKeyboardButton("🔹 Yᴇs 🔹", callback_data=f"yesigtv#{username}"),
+                    InlineKeyboardButton("🔹 ɴᴏ 🔹", callback_data=f"no#{username}")
                 ]
             ]
         )
